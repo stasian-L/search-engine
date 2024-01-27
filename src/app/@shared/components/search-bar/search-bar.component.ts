b99da408ddea5356c09ce35262f4318599f932c9
Stosian-L <lobach.stanislav@inbox.ru>
2024-01-27 22:53:32 +0300

    Add search-bar

--- src/app/@shared/components/search-bar/search-bar.component.ts
+++ src/app/@shared/components/search-bar/search-bar.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-search-bar',
     standalone: true,
-    imports: [],
+    imports: [MatInputModule, MatIconModule, ReactiveFormsModule, FormsModule],
     templateUrl: './search-bar.component.html',
     styleUrl: './search-bar.component.scss'
 })
 export class SearchBarComponent {
-
+  searchControl = new FormControl('hi');
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withDebugTracing } from '@angular/router';
 
-import { routes } from './app.routes';
 import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes, withDebugTracing()), provideAnimations(), BrowserAnimationsModule]
+    providers: [provideRouter(routes, withDebugTracing()), provideAnimations(), importProvidersFrom([BrowserAnimationsModule])]
 };
