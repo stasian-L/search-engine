0f9108976dcb984149070fdb40dd296867c092f0
Stosian-L <lobach.stanislav@inbox.ru>
2024-01-27 22:53:12 +0300

    Add search-bar

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SearchBarComponent } from '../../../@shared/components/search-bar/search-bar.component';
+import { ShortcutsComponent } from '../shortcuts/shortcuts.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [],
+    imports: [SearchBarComponent, ShortcutsComponent],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
---
+++ src/app/home/components/shortcuts/shortcuts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-shortcuts',
+    standalone: true,
+    imports: [],
+    templateUrl: './shortcuts.component.html',
+    styleUrl: './shortcuts.component.scss'
+})
+export class ShortcutsComponent {}
