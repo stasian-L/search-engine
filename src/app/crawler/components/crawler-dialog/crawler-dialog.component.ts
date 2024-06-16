af2c145a307938fc52ceea63db3a427b010ece5c
Stosian-L <lobach.stanislav@inbox.ru>
2024-06-16 23:58:25 +0300

    Fix crawler

--- src/app/crawler/components/crawler-dialog/crawler-dialog.component.ts
+++ src/app/crawler/components/crawler-dialog/crawler-dialog.component.ts
@@ -8,6 +8,7 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
 import { URL_REGEXP } from '../../../@core/constants/regex.const';
 
 @Component({
@@ -26,7 +27,8 @@ import { URL_REGEXP } from '../../../@core/constants/regex.const';
         MatIconModule,
         ReactiveFormsModule,
         MatCheckboxModule,
-        MatDividerModule
+        MatDividerModule,
+        MatTabsModule
     ],
     templateUrl: './crawler-dialog.component.html',
     styleUrl: './crawler-dialog.component.scss'
@@ -36,82 +38,75 @@ export class CrawlerDialogComponent implements AfterViewInit {
 
     private readonly fb = inject(FormBuilder);
 
-    urlsForm = this.fb.nonNullable.group({
+    urlForm = this.fb.nonNullable.group({
         useDepth: this.fb.control(false),
-        useSameDomain: this.fb.control(false),
         crawlDepth: this.fb.control({ value: 1, disabled: true }),
-        seedUrls: this.fb.nonNullable.array([this.fb.nonNullable.control('', [Validators.required, Validators.pattern(URL_REGEXP)])], this.ValidateDomainArray)
+        seedUrl: this.fb.control('', [Validators.required, Validators.pattern(URL_REGEXP)])
+    });
+
+    domainForm = this.fb.nonNullable.group({
+        seedUrls: this.fb.nonNullable.array(
+            [this.fb.nonNullable.control('', [Validators.required, Validators.pattern(URL_REGEXP)])],
+            validateDomainArray
+        )
     });
 
     get urlControls() {
-        return (this.urlsForm.get('seedUrls') as FormArray).controls;
+        return this.domainForm.controls.seedUrls.controls;
     }
 
     ngAfterViewInit(): void {
-        this.urlsForm.controls.useDepth.setValue(false);
+        this.urlForm.controls.useDepth.setValue(false);
     }
 
     addUrl(): void {
         const control = this.fb.control('', [Validators.required, Validators.pattern(URL_REGEXP)]);
-        (this.urlsForm.get('seedUrls') as FormArray).push(control);
+        (this.domainForm.get('seedUrls') as FormArray).push(control);
     }
 
     removeUrl(index: number) {
-        if (this.urlsForm.controls.seedUrls.length > 1) {
-            (this.urlsForm.get('seedUrls') as FormArray).removeAt(index);
-        }
-    }
-
-    onSameUrlValueChanged() {
-        if (this.urlsForm.controls.useSameDomain) {
-            //this.urlsForm.controls.seedUrls.addValidators(Validators)
+        if (this.domainForm.controls.seedUrls.length > 1) {
+            this.domainForm.controls.seedUrls.removeAt(index);
         }
     }
 
     toggleFormControl(controlName: string, value: boolean): void {
         if (value) {
-            this.urlsForm.get(controlName)?.enable();
+            this.urlForm.get(controlName)?.enable();
         } else {
-            this.urlsForm.get(controlName)?.disable();
+            this.urlForm.get(controlName)?.disable();
         }
     }
 
-    onSubmit(): void {
-        console.log(this.urlsForm.value);
-        if (this.urlsForm.invalid) {
+    onUrlSubmit(): void {
+        console.log(this.urlForm.value);
+        if (this.urlForm.invalid) {
             return;
         }
 
-        if (this.urlsForm.controls.useDepth) {
+        let depth = 1;
+        if (this.urlForm.controls.useDepth) {
+            depth = this.urlForm.controls.crawlDepth.value ?? 1;
         }
+        console.log({ urls: this.urlForm.controls.seedUrl.value, crawlDepth: depth });
 
-        if (this.urlsForm.controls.useSameDomain) {
-        }
-        console.log({ urls: this.urlsForm.controls.seedUrls.value, crawlDepth: this.urlsForm.controls.crawlDepth.value });
-
-        this.dialogRef.close({ seedUrls: this.urlsForm.controls.seedUrls.value, crawlDepth: 2 });
+        this.dialogRef.close({ seedUrls: [this.urlForm.controls.seedUrl.value], crawlDepth: depth });
     }
 
-    ValidateDomainArray() {
-        return (c: AbstractControl): { [key: string]: any } | null => {
-            if (this.getSameDomainUrls(c.value)) return null;
-
-            return { sameDomain: { valid: false } };
-        };
+    onDomainSubmit(): void {
+        console.log(this.domainForm.value);
+        if (this.domainForm.invalid) {
+            return;
+        }
+        console.log({ urls: this.domainForm.controls.seedUrls.value });
+        this.dialogRef.close({ seedUrls: this.domainForm.controls.seedUrls.value });
     }
 
     // private getSameDomainUrls(urls: string[]): boolean {
-    //     return urls
-    //         .filter(url => url.includes('://'))
-    //         .every(url => new URL(url).hostname === new URL(this.getSeedUrls()[0].value).hostname);
+    //     if (urls.length <= 1) return false;
+
+    //     const firstUrl = new URL(urls[0]);
+    //     return urls.every(url => new URL(url).hostname === firstUrl.hostname);
     // }
-
-
-    private getSameDomainUrls(urls: string[]): boolean {
-        if (urls.length === 0) return false;
-
-        const firstUrl = new URL(urls[0]);
-        return urls.every(url => new URL(url).hostname === firstUrl.hostname);
-    }
-
 }
+
